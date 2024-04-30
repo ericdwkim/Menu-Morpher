d3bb0d1575ac6e7b0033cedf6a9d48ccf8513f07
ericdwkim <ericdwkim.io@gmail.com>
2024-04-30 16:56:01 -0500

    fix [util]: syntax error for color `YELLOW`

--- utils/logger_config.py
+++ utils/logger_config.py
@@ -3,7 +3,7 @@ import logging
 
 class CustomFormatter(logging.Formatter):
     GREEN = "\033[0;32m"
-    YELLOW = "\E[33;47m"
+    YELLOW = "\033[33;47m"
     RED = "\033[1;31m"
     RESET = "\033[0;0m"
 
