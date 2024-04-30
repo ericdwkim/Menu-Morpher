08da2a51a3b0e8f182568dc62eb44e9d587fdeb0
ericdwkim <ericdwkim.io@gmail.com>
2024-04-30 16:45:52 -0500

    update [app]: minor logging changes for ux

--- app/__main__.py
+++ app/__main__.py
@@ -16,7 +16,7 @@ class App:
         setup_logger()
         self.credentials = get_credentials()
         self.account_id, self.location_id = get_account_and_location_ids()
-        logging.info(f'Account ID: {self.account_id}\nLocation ID: {self.location_id}')
+        logging.info(f'\n--------------------------------------------\nAccount ID: {self.account_id}\nLocation ID: {self.location_id}\n--------------------------------------------\n')
 
         # Create GMB API service
         self.service = build(
@@ -56,7 +56,7 @@ class App:
         canHaveFoodMenus_flag = location_metadata['metadata']['canHaveFoodMenus']
         # if flag is true, inform user to proceed with jsons to menu.json to then update()
         if canHaveFoodMenus_flag:
-            logigng.info(f'Based on your provided location ID: {self.location_id}, you may proceed with menu changes\ncanHaveFoodMenus_flag:{canHaveFoodMenus_flag}')
+            logging.info(f'Based on your location ID: "{self.location_id}", you can call update_food_menu via comamnd line flag `--update`\nEnsure that you\ve made all the appropriate changes to your menu.json prior to calling update.')
         else:
             logging.error(f'Based on your provided location ID: {self.location_id}, you will not be able to update food menus via updateFoodMenus API\ncanHaveFoodMenus_flag:{canHaveFoodMenus_flag}. See "https://developers.google.com/my-business/reference/rest/v4/accounts.locations" for more details')
 
